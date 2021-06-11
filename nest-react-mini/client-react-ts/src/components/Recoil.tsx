90571fe215562e1c43d728e5ba344bd07f1fb910
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2021-06-11 22:37:29 +0300

    refactored recoil

--- nest-react-mini/client-react-ts/src/components/Recoil.tsx
+++ nest-react-mini/client-react-ts/src/components/Recoil.tsx
@@ -11,15 +11,53 @@ function Recoil() {
 function CharacterCounter() {
   return (
     <div>
-      <p style={{ width: "80%", margin: "20px auto" }}>
-        Recoil lets you create a data-flow graph that flows from atoms (shared
-        state) through selectors (pure functions) and down into your React
-        components. Atoms are units of state that components can subscribe to.
-        Selectors transform this state either synchronously or asynchronously.
-      </p>
-      <h4>Recoil's Counter App:</h4>
+      <RecoilAbout />
       <TextInput />
       <CharacterCount />
+      <RecoilDocs />
+    </div>
+  );
+}
+
+const textState = atom({
+  key: "textState", // unique ID (with respect to other atoms/selectors)
+  default: "", // default value (aka initial value)
+});
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event: any) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <h4>Recoil's Counter App:</h4>
+      <input type="text" value={text} onChange={onChange} />
+      <p>Echo: {text}</p>
+    </div>
+  );
+}
+
+const charCountState = selector({
+  key: "charCountState", // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <p>Character Count: {count}</p>;
+}
+
+function RecoilDocs() {
+  return (
+    <>
       <h4>Recoil Docs:</h4>
       <a
         style={{ display: "block" }}
@@ -45,43 +83,21 @@ function CharacterCounter() {
       >
         Development Tools
       </a>
-    </div>
+    </>
   );
 }
 
-const textState = atom({
-  key: "textState", // unique ID (with respect to other atoms/selectors)
-  default: "", // default value (aka initial value)
-});
-
-function TextInput() {
-  const [text, setText] = useRecoilState(textState);
-
-  const onChange = (event: any) => {
-    setText(event.target.value);
-  };
-
+function RecoilAbout() {
   return (
-    <div>
-      <input type="text" value={text} onChange={onChange} />
-      <p>Echo: {text}</p>
-    </div>
+    <>
+      <p style={{ width: "80%", margin: "20px auto" }}>
+        Recoil lets you create a data-flow graph that flows from atoms (shared
+        state) through selectors (pure functions) and down into your React
+        components. Atoms are units of state that components can subscribe to.
+        Selectors transform this state either synchronously or asynchronously.
+      </p>
+    </>
   );
-}
-
-const charCountState = selector({
-  key: "charCountState", // unique ID (with respect to other atoms/selectors)
-  get: ({ get }) => {
-    const text = get(textState);
-
-    return text.length;
-  },
-});
-
-function CharacterCount() {
-  const count = useRecoilValue(charCountState);
-
-  return <p>Character Count: {count}</p>;
 }
 
 export default Recoil;
