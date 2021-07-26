3bdacbc0cb13fc1393f099542942cefbe26e816e
Serhiy Stepanenko <serhiy.stepanenko@newfireglobal.com>
2021-07-26 16:45:34 +0300

    updated nest packages +rabbitMQ

--- nest-react-mini/server-nest/src/app.module.ts
+++ nest-react-mini/server-nest/src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+
 
 @Module({
-  imports: [CarsModule],
+  imports: [
+    CarsModule,
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'exchange1',
+          type: 'topic',
+        },
+      ],
+      uri: 'amqp://rabbitmq:rabbitmq@localhost:5672',
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
+
+// More on Rabbit: https://github.com/golevelup/nestjs/tree/master/packages/rabbitmq
